4e90254083e95938cd88e31c7a5cb8061e9faae4
Lars Reimann <mail@lars-reimann.eu>
2014-02-28 22:01:33 +0100

    Added a couple of variables.

--- game_controller.js
+++ game_controller.js
@@ -5,7 +5,12 @@ var GameController = (function() {
      * Constructs a new GameController object.
      */
     var init = function() {
+        var lives; // TODO: Initialization (config file, constant)
         var score = 0;
+
+        this.get_lives = function() {
+            return lives;
+        }
 
         this.get_score = function() {
             return score;
--- tower.js
+++ tower.js
@@ -5,7 +5,11 @@ var Tower = (function() {
      * Constructs a new Tower object.
      */
     var init = function() {
+        var hitpoints; // TODO: Initialization (config file, constant)
 
+        var get_hitpoints = function() {
+            return hitpoints;
+        };
     };
 
     return init;
