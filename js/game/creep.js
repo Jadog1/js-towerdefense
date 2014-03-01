38be0059c6e840f0c9fbd87a297c84dfba623402
Lars Reimann <mail@lars-reimann.eu>
2014-03-01 16:47:52 +0100

    Added variables and getters to Creep.

--- js/game/creep.js
+++ js/game/creep.js
@@ -4,9 +4,24 @@ var Creep = (function() {
     /**
      * Constructs a new Creep object.
      */
-    var init = function() {
+    var init = function(data) {
+        var coordinates = data.coordinates;
+        var current_hitpoints = data.hitpoints;
+        var movement = data.movement;
+        var initial_hitpoints = data.hitpoints;
 
+        this.get_coordinates = function() {
+            return Point.copy(coordinates); // defensive copying
+        };
+
+        this.get_current_hitpoints = function() {
+            return current_hitpoints;
+        };
+
+        this.get__initial_hitpoints = function() {
+            return initial_hitpoints;
+        };
     };
 
     return init;
-}());
+})();
